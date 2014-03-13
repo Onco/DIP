6822b8138b3ce745d0c6eb267066d74b510da88f
Ondrej Vavro <ondrej.vavro@gmail.com>
2014-03-13 01:34:32 +0100

    Initial commit of randomizer.py

---
+++ randomizer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+############################################
+# Name: randomizer.py                      #
+# Autor: Ondrej Vavro                      #
+# Description: Randomly selects specified  #
+# number of images from the provided video #
+# file.                                    #
+# Date of last modification: 6 March 2014  #
+############################################
+
+import sys, os, re, optparse
+
+def main():
+	return	# main()
+
+if __name__ = "__main__":
+	main()
