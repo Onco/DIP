e375cca5ba7dc91795d766e9b6fe8c9edd58cd29
Onco <ondrej.vavro@gmail.com>
2014-03-24 06:11:35 +0100

    Parser change to randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -8,9 +8,13 @@
 # Date of last modification: 6 March 2014  #
 ############################################
 
-import sys, os, re, optparse
+import sys, os, re, argparse, cv2
 
 def main():
+	parser = argparse.ArgumentParser(description='Randomly selects specified number of images from a provided video file.')
+	parser.add_argument('video_file')
+	parser.add_argument('-n', '--num', nargs='?', const='1', default='1', type='int')
+	
 	return	# main()
 
 if __name__ = "__main__":
