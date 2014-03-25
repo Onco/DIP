c78896827287ebc27f2abdcb6aa92efe4b6fa032
Ondrej Vavro <ondrej.vavro@gmail.com>
2014-03-25 22:19:49 +0100

    Randomizer extracting images, 1st usable version.

--- randomizer.py
+++ randomizer.py
@@ -8,20 +8,27 @@
 # Date of last modification: 6 March 2014  #
 ############################################
 
-import sys, os, re, argparse#, cv2
+import sys, os, re, argparse, cv2, random
 
 def main():
 	parser = argparse.ArgumentParser(description='Randomly selects specified number of images from a provided video file.')
-	parser.add_argument('video_file', nargs=1, help='Video file to be processed.')
-	parser.add_argument('-n', '--num', nargs=1, default='1', type=int, help='Number of images to be extracted. (default is 1)')
+	parser.add_argument('video_file', nargs='?', help='Video file to be processed.')
+	parser.add_argument('-n', '--num', nargs='?', default='1', type=int, help='Number of images to be extracted. (default is 1)')
+	parser.add_argument('-o', '--outname', nargs='?', default="IMG_", help='Prefix of the output image files. (default is "IMG_")')
 	args = parser.parse_args()
 
-	print options
-	print args
 
-	#video = cv2.VideoCapture(video_file)
-	
-	#while()
+	video = cv2.VideoCapture(args.video_file)
+	numf = video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+
+	for im in range(0,args.num):
+		video.set(cv2.cv.CV_CAP_PROP_FRAME_COUNT,random.randint(0, numf))
+		ret, image = video.read()
+		cv2.imwrite("%(name)s%(count)d.jpg" % {"name": args.outname, "count": im}, image)
+		
+
+        video.release()        
+
 	return	# main()
 
 if __name__ == "__main__":
